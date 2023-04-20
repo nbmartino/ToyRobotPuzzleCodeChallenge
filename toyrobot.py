bcd4d9d9e5015f1fa84aab160d64ded97cbc250b
Neil Martino <nbmartino@gmail.com>
2023-04-20 19:58:49 +0800

    First

---
+++ toyrobot.py
@@ -0,0 +1,26 @@
+class ToyRobot:
+
+    currPos = None
+    currDir = None
+
+    def IsPlacedOnBoard():
+        pass
+
+    def GoTo(x, y, direction = None):
+        pass
+    
+    def Move():
+        pass
+    
+    def AnnouncePosition():
+        pass
+
+    def Reset():
+        pass
+
+    def RotateLeft():
+        pass
+
+    def RotateRight():
+        pass
+    
